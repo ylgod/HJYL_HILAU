feca2ae372b30a7920fe5ea379ea215ad02984d8
皇家元林 <i@hjyl.org>
2019-10-14 11:29:58 +0800

    Update page-go.php

--- page-go.php
+++ page-go.php
@@ -2,23 +2,42 @@
 /*
 Template Name: go跳转页面
 */
-if(strlen($_SERVER['REQUEST_URI']) > 384 || strpos($_SERVER['REQUEST_URI'], "eval(") || strpos($_SERVER['REQUEST_URI'], "base64")) {
-    @header("HTTP/1.1 414 Request-URI Too Long");
-    @header("Status: 414 Request-URI Too Long");
-    @header("Connection: Close");
-    @exit;
+//自定义跳转地址
+$cars = array(
+   array("qcloud",'https://hjyl.org/'),
+   array("upyun",'https://hjyl.org/'),
+   array("trustutn",'https://hjyl.org/'),
+   array("gfvps",'https://hjyl.org/'),
+   array("360scan",'https://hjyl.org/')
+   );
+
+if(strlen($_SERVER['REQUEST_URI']) > 384 ||
+    strpos($_SERVER['REQUEST_URI'], "eval(") ||
+    strpos($_SERVER['REQUEST_URI'], "base64")) {
+        @header("HTTP/1.1 414 Request-URI Too Long");
+        @header("Status: 414 Request-URI Too Long");
+        @header("Connection: Close");
+        @exit;
 }
 //通过QUERY_STRING取得完整的传入数据，然后取得url=之后的所有值，兼容性更好
 $t_url = htmlspecialchars(preg_replace('/^url=(.*)$/i','$1',$_SERVER["QUERY_STRING"]));
+
+//此处可以自定义一些特别的外链，不需要可以删除以下5行
+foreach($cars as $k=>$val){
+    if($t_url==$val[0] ) {
+      $t_url =  $val[1];
+      $t_vip = 1;
+    }
+}
  
 //数据处理
 if(!empty($t_url)) {
     //判断取值是否加密
     if ($t_url == base64_encode(base64_decode($t_url))) {
-        $t_url = base64_decode($t_url);
+        $t_url =  base64_decode($t_url);
     }
- //对取值进行网址校验和判断
-preg_match('/^(http|https|thunder|qqdl|ed2k|Flashget|qbrowser):\/\//i',$t_url,$matches);
+    //对取值进行网址校验和判断
+    preg_match('/^(http|https|thunder|qqdl|ed2k|Flashget|qbrowser):\/\//i',$t_url,$matches);
 if($matches){
     $url=$t_url;
     $title='页面加载中,请稍候...';
